e02338c695a879c03601f08cc8f7ae0e55c792ff
Thomas Burton <twyburton@outlook.com>
2015-08-07 13:01:28 +0100

    Update bubblePlotScript.R

--- bubblePlotScript.R
+++ bubblePlotScript.R
@@ -1,30 +1,37 @@
-bubblePlot <- function( table , xCol, yCol, gridSize , cellMin ){
+bubblePlot <- function( table , xCol, yCol, xGridSize, yGridSize , cellMin ){
 
 	# Prepare inputs
-	gridSize <- floor(gridSize)
+	xGridSize <- floor(xGridSize)
+	yGridSize <- floor(yGridSize)
+
 
 	# Get max and min of axis
-	xMax <- max(table[, xCol ], na.rm = TRUE)
-	xMin <- min(table[, xCol ], na.rm = TRUE)
-	yMax <- max(table[, yCol ], na.rm = TRUE)
-	yMin <- min(table[, yCol ], na.rm = TRUE)
+	xMax <- ceiling(max(table[, xCol ], na.rm = TRUE))
+	xMin <- floor(min(table[, xCol ], na.rm = TRUE))
+	yMax <- ceiling(max(table[, yCol ], na.rm = TRUE))
+	yMin <- floor(min(table[, yCol ], na.rm = TRUE))
 
 	# Create grid for plot
 	plot_width <- ceiling(xMax - xMin)
 	plot_height <- ceiling(yMax - yMin)
 	
-	xCells <- plot_width / gridSize
-	yCells <- plot_height / gridSize
+	xCells <- ceiling( plot_width / xGridSize + 1)
+	yCells <- ceiling( plot_height / yGridSize  + 1)
 
 	# Create array
 	data_array <- array()
 	
 	# Go though each row of table and add 1 to cell where values fall in bounds 
 	for ( i in 1: nrow(table) ) {
-		x_pos = floor( ( table[ i , xCol ] - xMin ) / gridSize )
-		y_pos = floor( ( table[ i , yCol ] - yMin ) / gridSize )
+		x_pos = floor( ( table[ i , xCol ] - xMin ) / xGridSize )
+		y_pos = floor( ( table[ i , yCol ] - yMin ) / yGridSize )
 
-		array_index <- x_pos + (plot_width * y_pos) + 1
+		#x_pos = floor( ( table[ i , xCol ] - xMin ) / xCells )
+		#y_pos = floor( ( table[ i , yCol ] - yMin ) / yCells )
+
+
+		array_index <- x_pos + (xCells * y_pos ) + 1
+		#array_index <- x_pos + (plot_width * y_pos ) + 1
 	
 		if ( is.na( data_array[ array_index ] ) ) {
 			data_array[array_index ] <- 1
@@ -34,16 +41,15 @@ bubblePlot <- function( table , xCol, yCol, gridSize , cellMin ){
 	}
 
 	# Create table to plot
-
 	x_plot <- array()
 	y_plot <- array()
 	frequency_plot <- array()
 
 	for ( i in 1:length( data_array ) ){
-		x_plot[i] <- xMin + (i %% plot_width) + (gridSize/2)
-		y_plot[i] <- yMin + floor( i / plot_width ) + (gridSize/2)
+		x_plot[i] <- xMin + ( (i-1) %% xCells) * xGridSize + (xGridSize/2)
+		y_plot[i] <- yMin + floor( (i-1) / xCells ) * yGridSize + (yGridSize/2)
 		if( is.na(data_array[i] ) || data_array[i] < cellMin){
-			frequency_plot[i] <- 0
+			frequency_plot[i] <- NA
 		} else {
 			frequency_plot[i] <- data_array[i]
 		}
@@ -51,11 +57,10 @@ bubblePlot <- function( table , xCol, yCol, gridSize , cellMin ){
 	}
 
 	symbols(x_plot,y_plot,frequency_plot, xlab = xCol, ylab = yCol)
+	grid()
 
 }
 
 
-# raw_data <- read.csv("O:\\\\Documents\\Data\\ALSPAC\\ALSPAC.csv", header=TRUE )  # read csv file 
-# bubblePlot( raw_data , "BMI.7" , "wt.7", 1 , 5 )
-
-
+raw_data <- read.csv("O:\\\\Documents\\Data\\ALSPAC\\ALSPAC.csv", header=TRUE )  # read csv file 
+bubblePlot( raw_data , "BMI.7" , "wt.7", 2 , 2 , 5 )
